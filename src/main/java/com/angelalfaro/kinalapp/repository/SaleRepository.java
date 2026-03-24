be75a0f2a8de15440be3fdcf0a05db8c236157c8
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-03-24 17:25:29 -0600

    feat: adding SaleRepository

---
+++ src/main/java/com/angelalfaro/kinalapp/repository/SaleRepository.java
@@ -0,0 +1,15 @@
+package com.angelalfaro.kinalapp.repository;
+
+import com.angelalfaro.kinalapp.entity.Sale;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, UUID> {
+
+    Slice<Sale> findByStateSale(int state);
+
+}
