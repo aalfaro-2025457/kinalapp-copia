1c123ed6455159fcc198a3b63751d42c99e2963d
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-03-27 10:55:53 -0600

    refactor: adding @JsonIgnoreProperties in @ManyToOne relations in Sale Entity

--- src/main/java/com/angelalfaro/kinalapp/entity/Sale.java
+++ src/main/java/com/angelalfaro/kinalapp/entity/Sale.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Data
 @Entity
 @Table(name = "sales")
@@ -25,10 +27,12 @@ public class Sale {
     @Column(nullable = false)
     private int stateSale;
 
+    @JsonIgnoreProperties("salesClient")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dpi_client", foreignKey = @ForeignKey(name = "FK_client"), nullable = false)
     private Client clientSale;
 
+    @JsonIgnoreProperties("salesUser")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "codeUser", foreignKey = @ForeignKey(name = "FK_user"), nullable = false)
     private User userSale;
