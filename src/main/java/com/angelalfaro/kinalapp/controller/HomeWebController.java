5283f3ad2585aad4f438cb4866f82b3b0724505a
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-15 17:03:15 -0600

    refactor: products crud is now functional

--- src/main/java/com/angelalfaro/kinalapp/controller/HomeWebController.java
+++ src/main/java/com/angelalfaro/kinalapp/controller/HomeWebController.java
@@ -24,7 +24,7 @@ public class HomeWebController {
     
     @GetMapping("/products")
     public String goProducts() {
-        return "cruds/products";
+        return "redirect:/view/products";
     }
 
     @GetMapping("/sales")
