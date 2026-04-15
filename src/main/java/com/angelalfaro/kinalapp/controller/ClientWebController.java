d43abd2493316bb40e64241267c524de66dd1e39
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-15 16:25:53 -0600

    refactor: add clients form functional

--- src/main/java/com/angelalfaro/kinalapp/controller/ClientWebController.java
+++ src/main/java/com/angelalfaro/kinalapp/controller/ClientWebController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,7 +24,14 @@ public class ClientWebController {
 
         List<Client> clients = clientService.listAll();
         model.addAttribute("clients", clients);
+        model.addAttribute("newClient", new Client());
         return "cruds/clients";
+    }
+
+    @PostMapping("/save")
+    public String saveClient(@ModelAttribute("newClient") Client client) {
+        clientService.save(client);
+        return "redirect:/view/clients";
     }
 
 }
