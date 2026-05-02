77dad30042e047f46e6297db792be97e07ea0fcb
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-05-01 19:58:17 -0600

    refactor: configure "/register" permitAll

--- src/main/java/com/angelalfaro/kinalapp/config/SecurityConfig.java
+++ src/main/java/com/angelalfaro/kinalapp/config/SecurityConfig.java
@@ -1,21 +1,32 @@
 package com.angelalfaro.kinalapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.angelalfaro.kinalapp.entity.User;
+import com.angelalfaro.kinalapp.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserRepository userRepository;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/styles/**","/scripts/**","/login").permitAll()
+                        .requestMatchers("/styles/**","/scripts/**","/login","/register").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
@@ -32,6 +43,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
